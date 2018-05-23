23d72fb0e3782300e4aacb75f7c6922070059ce2
Gonzalo Rocha <grochadc@gmail.com>
2018-05-23 15:34:54 -0500

    Add all question data to its own json file

--- src/components/Test.js
+++ src/components/Test.js
@@ -1,46 +1,23 @@
 import React, {Component} from 'react';
 import Question from './Question';
+import Data from '../database/Questions.json';
+
+
+const questions = Data.questions; //From Questions.json
 
 class Test extends Component {
   constructor(props){
     super(props);
-    this.state = {
-        questions: [
-                { title: "Jessica _______ a good dancer.",
-                  options: [
-                    {text: 'are', correct: false},
-                    {text: 'be', correct: false},
-                    {text:'not be', correct: false},
-                    {text: 'is', correct: true}
-                  ]
-                },
-                { title: "What is it? It's a____ .",
-                  options: [
-                    {text: 'e-book', correct: false},
-                    {text: 'cellphone', correct: true},
-                    {text:'umbrella', correct: false},
-                    {text: 'watches', correct: false}
-                  ]
-                },
-                { title: "A: ______ you Mexican? \n B: Yes, I am.",
-                  options: [
-                    {text: 'am', correct: false},
-                    {text: 'are', correct: true},
-                    {text: 'be', correct: false},
-                    {text: 'is', correct: false}
-                  ]
-                }
-              ]
-    };
     this.checkAnswer = this.checkAnswer.bind(this);
   }
   checkAnswer(question, answer){
-    console.log(this.state.questions[question].options[answer].correct);
+    console.log('Answering: ',question);
+    console.log(questions[question].options[answer].correct);
   }
   render() {
     return (
       <div className="test">
-        {this.state.questions.map((question, i) => <Question info={question} index={i} key={i} checkAnswer={this.checkAnswer}/>)}
+        {questions.map((question, i) => <Question info={question} index={i} key={i} checkAnswer={this.checkAnswer}/>)}
       </div>
     );
   }
