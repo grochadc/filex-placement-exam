346cbf70951657a660e332a7875d860b4d25c4b5
Gonzalo Rocha <grochadc@gmail.com>
2018-05-23 17:24:07 -0500

    Add submit button to check total correct answers

--- src/components/Test.js
+++ src/components/Test.js
@@ -1,3 +1,4 @@
+/*jshint esversion: 6*/
 import React, {Component} from 'react';
 import Question from './Question';
 import Data from '../database/Questions.json';
@@ -8,16 +9,33 @@ const questions = Data.questions; //From Questions.json
 class Test extends Component {
   constructor(props){
     super(props);
+    this.state = {
+      answers: new Array(questions.length)
+    };
     this.checkAnswer = this.checkAnswer.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
   }
+
   checkAnswer(question, answer){
-    console.log('Answering: ',question);
-    console.log(questions[question].options[answer].correct);
+    let correct = questions[question].options[answer].correct;
+    let newAnswers = [
+      ...this.state.answers.slice(0,question),
+      correct,
+      ...this.state.answers.slice(question+1)
+    ];
+
+    this.setState({answers: newAnswers});
   }
+
+  handleSubmit() {
+    alert(this.state.answers.filter(v => v).length);
+  }
+
   render() {
     return (
       <div className="test">
-        {questions.map((question, i) => <Question info={question} index={i} key={i} checkAnswer={this.checkAnswer}/>)}
+        {questions.map((question, i) => <Question info={question} index={i} key={i} checkAnswer={this.checkAnswer} />)}
+        <button onClick={this.handleSubmit}>Submit</button>
       </div>
     );
   }
